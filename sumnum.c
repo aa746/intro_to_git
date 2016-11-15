f65a37cdb14e66c6398cdbeac6dcf023339ad3bf
aa746 <aa746@exeter.ac.uk>
2016-11-15 14:22:29 +0000

    Revert "Changing variable names"
    This reverts commit ffb74d0b60b9378abc1d3288cc7bbea5a2b46dc0.

--- sumnum.c
+++ sumnum.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int n1, n2, sumOfTwoNumbers;
+    int n1, n2, Sum;
     
     printf("Enter two integers: ");
 
@@ -9,15 +9,10 @@ int main()
     scanf("%d %d", &n1, &n2);
 
     // store sum in variable Sum
-    sumOfTwoNumbers = n1 + n2;
+    Sum = n1 + n2;
 
     // display output     
-    printf("%d + %d = %d", n1, n2, sumOfTwoNumbers);
-	
-	 // check if the sum is even or odd, 
-    // and print result to screen
-    if(sumOfTwoNumbers % 2 == 0) printf("%d is even\n", sumOfTwoNumbers);
-    else printf(" %d is odd\n", sumOfTwoNumbers);
+    printf("%d + %d = %d", n1, n2, Sum);
 
     return 0;
 }
